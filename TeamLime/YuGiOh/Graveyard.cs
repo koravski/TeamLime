66d56e7cdfc7abcd826952b2e1c9f9bdecac51d5
PavDragneva <pavlinadragneva@yahoo.com>
2015-03-25 15:18:37 +0200

    Created graveyard

---
+++ TeamLime/YuGiOh/Graveyard.cs
@@ -0,0 +1,39 @@
+﻿namespace YuGiOh
+{
+    using System;
+    using YuGiOh.Cards;
+    using System.Collections.Generic;
+    public abstract class Graveyard
+    {
+        private List<Card> cardsInGraveyard = new List<Card>();
+        private int numberOfCardsInGraveyard;
+
+        public List<Card> CardsInGraveyard
+        {
+            get
+            {
+                return this.cardsInGraveyard;
+            }
+            set
+            {
+                this.CardsInGraveyard = value;
+            }
+        }
+        public int NumberOfCardsInGraveyard
+        {
+            get
+            {
+                return CardsInGraveyard.Count;
+            }
+        }
+
+        public void SendCardToGraveyard(Card card)
+        {
+            CardsInGraveyard.Add(card);
+        }
+        public void RemoveCardFromGraveyard(Card card)
+        {
+            CardsInGraveyard.Remove(card);
+        }
+    }
+}
