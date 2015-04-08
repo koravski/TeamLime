26cd3869cb63f9f93d52c7f0ccbc6e410e264f5c
VProfirov <Vasil.Profirov@gmail.com>
2015-04-08 09:05:41 +0300

    Helper,Constants,PlayerOneMoves,PlayerTwoMoves...

---
+++ TeamLime-master/TeamLime/YuGiOh/Extensions/Helper.cs
@@ -0,0 +1,26 @@
+﻿
+//// It should be in its own solution - but for the moment
+namespace YuGiOh.Extensions.Helper
+{
+    using System;
+
+    using System.Linq;
+
+    public abstract class Constants
+    {
+        public const string CardFilePathMonster = "../CardsPhoto/Monster/";
+        public const string CardFilePathSpell = "../CardsPhoto/Spell/";
+        public const string CardFilePathTrap = "../CardsPhoto/Trap/";
+
+        public const string CardFileExtension = ".jpg";
+
+        public const string HighScoreFilePath = " ";
+        public const string PlayerOneMoves = "../Extensions/PlayersMoves/PlayerOneMoves.txt";
+        public const string PlayerTwoMoves = "../Extensions/PlayersMoves/PlayerTwoMoves.txt";
+
+        public const string GameIntro = " ";
+        public const string GameBoard = " ";
+        public const string GameMenuIntro = " ";
+        public const string GameRules = " ";
+    }
+}
